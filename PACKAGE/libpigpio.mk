8086ece81b19e3f72c8dc378cc498cde92fea940
ciclonite <ciclonite@gmail.com>
2015-10-28 14:05:24 +0100

    Edit files for correct version number

--- PACKAGE/libpigpio.mk
+++ PACKAGE/libpigpio.mk
@@ -1,11 +1,9 @@
-LIBPIGPIO_VERSION = 6
+LIBPIGPIO_VERSION = 39
 LIBPIGPIO_INSTALL_TARGET = YES
 LIBPIGPIO_INSTALL_STAGING = YES
 LIBPIGPIO_AUTORECONF = YES
-LIBPIGPIO_SITE = http://github.com/limpens/pigpio/tarball/master
+LIBPIGPIO_SITE = http://github.com/ciclonite/pigpio/tarball/master
 
-ifeq ($(BR2_PACKAGE_LIBPIGPIO),y)
-TARGETS+=libpigpio
-endif
+
 $(eval $(autotools-package))
 
